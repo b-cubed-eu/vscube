ffa332b0576947fe79c81a41cb4a0abb1a0cdb5e
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2026-02-05 15:45:10 +0100

    Update example_data.R

--- R/example_data.R
+++ R/example_data.R
@@ -2,7 +2,7 @@
 #' @return occurrence.txt file
 #' @export
 #' @examples
-#' example_dataset()
+#' example_data()
 example_data <- function() {
 file <- system.file("inst", "occurrence.txt", package = "vscube") 
 return(file)
